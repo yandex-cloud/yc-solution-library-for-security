73a78e7dcbc39479c3283d1526caa9cb93e91cfa
Kirill Shevchuk <knpsh@outlook.com>
2021-08-16 13:15:59 +0000

    bytes to string fix while decoding

--- auditlogs/export-k8s-events-to-siem/security-events-to-siem-importer/worker/function/main.py
+++ auditlogs/export-k8s-events-to-siem/security-events-to-siem-importer/worker/function/main.py
@@ -19,7 +19,8 @@ def decrypt_secret_kms(secret):
     request_suffix      = kms_key_id+':decrypt'
     request_json_data   = {'ciphertext': secret}
     response            = requests.post('https://kms.yandex/kms/v1/keys/'+request_suffix, data=json.dumps(request_json_data), headers={"Accept":"application/json", "Authorization": "Bearer "+token})
-    return base64.b64decode(response.json().get('plaintext'))
+    b64_data            = response.json().get('plaintext')
+    return base64.b64decode(b64_data).decode()
 
 
 # Configuration - Keys
