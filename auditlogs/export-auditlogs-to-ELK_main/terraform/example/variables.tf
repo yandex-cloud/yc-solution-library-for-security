75e214c3761cdd33d7cf1ab5dd92ddadac96902e
Kirill Shevchuk <knpsh@outlook.com>
2021-09-02 14:26:11 +0600

    clean-up

--- auditlogs/export-auditlogs-to-ELK_main/terraform/example/variables.tf
+++ auditlogs/export-auditlogs-to-ELK_main/terraform/example/variables.tf
@@ -2,17 +2,17 @@
 
 variable "token" {
   description = "Yandex Cloud security OAuth token"
-  default     = "/Users/mirtov8/Desktop/key.json" #generate yours by this https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token
+  default     = "key.json" # generate yours by this https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token
 }
 
 variable "folder_id" {
   description = "Yandex Cloud Folder ID where resources will be created"
-  default     = "b1gci8pu7s2seup3mpor" #yc config get folder-id
+  default     = "xxxxxx" # yc config get folder-id
 }
 
 variable "cloud_id" {
   description = "Yandex Cloud ID where resources will be created"
-  default     = "b1g3o4minpkuh10pd2rj" #yc config get cloud-id
+  default     = "xxxxxx" # yc config get cloud-id
 }
 
 
