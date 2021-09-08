94901d7652cf8deab423d2101d37b40cd62c1391
Kirill Shevchuk <knpsh@outlook.com>
2021-09-09 02:11:45 +0600

    minor editing

--- auditlogs/export-auditlogs-to-ELK_main/terraform/modules/yc-elastic-trail/main.tf
+++ auditlogs/export-auditlogs-to-ELK_main/terraform/modules/yc-elastic-trail/main.tf
@@ -1,16 +1,15 @@
-//Импортим sa
+# Сервисная учетная запись
 data "yandex_iam_service_account" "bucket_sa" {
   service_account_id = var.sa_id
 }
 
-
-//Создаем static key
+# Создаем static key
 resource "yandex_iam_service_account_static_access_key" "sa_static_key" {
   service_account_id = data.yandex_iam_service_account.bucket_sa.id
   description        = "static access key for object storage"
 }
 
-//Работаем с ssh ключем
+# Работаем с ssh ключем
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
   rsa_bits  = "4096"
@@ -23,41 +22,41 @@ resource "local_file" "private_key" {
 }
 
 data "template_file" "cloud_init_lin" {
-  template = file("../modules/yc-elastic-trail/cloud-init_lin.tpl.yaml")
-   vars =  {
-        ssh_key = "${chomp(tls_private_key.ssh.public_key_openssh)}"
-    }
+  template  = file("../modules/yc-elastic-trail/cloud-init_lin.tpl.yaml")
+  vars      =  {
+    ssh_key = "${chomp(tls_private_key.ssh.public_key_openssh)}"
+  }
 }
 
-//Создаем docker-declaration
+# Создаем docker-declaration
 data "template_file" "docker-declaration" {
-  template = file("../modules/yc-elastic-trail/docker-declaration.yaml")
-    vars =  {
-        ELASTIC_SERVER = "${var.elk_address}:9200"
-        KIBANA_SERVER = "${var.elk_address}"
-        ELASTIC_AUTH_USER = "admin"
-        ELASTIC_INDEX_NAME = "audit-trails-index"
-        S3_BUCKET = "${var.bucket_name}"
-        S3_FOLDER = "${var.bucket_folder}"
-        SLEEP_TIME = "300"
-        ELK_PASS_ENCR = "${yandex_kms_secret_ciphertext.encrypted_pass.ciphertext}"
-        S3_KEY_ENCR = "${yandex_kms_secret_ciphertext.encrypted_s3_key.ciphertext}"
-        S3_SECRET_ENCR = "${yandex_kms_secret_ciphertext.encrypted_s3_secret.ciphertext}"
-        KMS_KEY_ID = "${yandex_kms_symmetric_key.key-elk.id}"
-    }
+  template  = file("../modules/yc-elastic-trail/docker-declaration.yaml")
+  vars      =  {
+    ELASTIC_SERVER      = "${var.elk_address}:9200"
+    KIBANA_SERVER       = "${var.elk_address}"
+    ELASTIC_AUTH_USER   = "admin"
+    ELASTIC_INDEX_NAME  = "audit-trails-index"
+    S3_BUCKET           = "${var.bucket_name}"
+    S3_FOLDER           = "${var.bucket_folder}"
+    SLEEP_TIME          = "300"
+    ELK_PASS_ENCR       = "${yandex_kms_secret_ciphertext.encrypted_pass.ciphertext}"
+    S3_KEY_ENCR         = "${yandex_kms_secret_ciphertext.encrypted_s3_key.ciphertext}"
+    S3_SECRET_ENCR      = "${yandex_kms_secret_ciphertext.encrypted_s3_secret.ciphertext}"
+    KMS_KEY_ID          = "${yandex_kms_symmetric_key.key-elk.id}"
+  }
 }
 
-
-//Развертывание Container-optimised image
+# Развертывание Container-Optimised Image
 data "yandex_compute_image" "container-optimized-image" {
   family = "container-optimized-image"
 }
 
 resource "yandex_compute_instance" "instance-based-on-coi" {
-  name        = "elk-sync"
-  hostname    = "elk-sync"
-  zone        = "ru-central1-a"
-  service_account_id = data.yandex_iam_service_account.bucket_sa.id
+  name                = "elk-sync"
+  hostname            = "elk-sync"
+  zone                = "ru-central1-a"
+  service_account_id  = data.yandex_iam_service_account.bucket_sa.id
+  
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.container-optimized-image.id
@@ -65,33 +64,33 @@ resource "yandex_compute_instance" "instance-based-on-coi" {
       size     = 100
     }
   }
+
   network_interface {
-    subnet_id  = var.coi_subnet_id
-    #не забыть включить NAT для subnet, где COI 
+    subnet_id  = var.coi_subnet_id # Не забудьте включить NAT для подсети, где будет размещен COI! 
   }
 
   resources {
-    cores = 4
-    memory = 4
+    cores   = 4
+    memory  = 4
   }
-  metadata = {
-  user-data = "${data.template_file.cloud_init_lin.rendered}"
-  docker-container-declaration = "${data.template_file.docker-declaration.rendered}"
-}
+
+  metadata  = {
+    user-data = "${data.template_file.cloud_init_lin.rendered}"
+    docker-container-declaration = "${data.template_file.docker-declaration.rendered}"
+  }
 }
 
-//Создание KMS ключа
+# Создание KMS ключа
 resource "yandex_kms_symmetric_key" "key-elk" {
   name              = "key-elk"
   description       = "description for key"
   default_algorithm = "AES_128"
 }
 
-//Назначение роли на sa на расшифровку ключа
+# Назначение роли на sa на расшифровку ключа
 resource "yandex_resourcemanager_folder_iam_binding" "binding" {
   folder_id = var.folder_id
-
-  role = "kms.keys.encrypterDecrypter"
+  role      = "kms.keys.encrypterDecrypter"
 
   members = [
     "serviceAccount:${data.yandex_iam_service_account.bucket_sa.id}",
