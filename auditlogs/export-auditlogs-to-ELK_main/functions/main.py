2fe6c18b577f407350eed1dca1e6e530a5705317
Kirill Shevchuk <knpsh@outlook.com>
2021-09-09 03:49:22 +0600

    added alias, ilm, index-template

--- auditlogs/export-auditlogs-to-ELK_main/functions/main.py
+++ auditlogs/export-auditlogs-to-ELK_main/functions/main.py
@@ -5,10 +5,12 @@ import boto3
 import time
 import base64
 
+
 # Function - Get token
 def get_token():
     response = requests.get('http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token', headers={"Metadata-Flavor":"Google"})
     return response.json().get('access_token')
+
 
 # Function - Decrypt data with KMS key
 def decrypt_secret_kms(secret):
@@ -18,6 +20,7 @@ def decrypt_secret_kms(secret):
     response = requests.post('https://kms.yandex/kms/v1/keys/'+request_suffix, data=json.dumps(request_json_data), headers={"Accept":"application/json", "Authorization": "Bearer "+token})
     b64_data = response.json().get('plaintext')
     return base64.b64decode(b64_data).decode()
+
 
 # Configuration - Get ElasticSearch CA.pem
 def get_elastic_cert():
@@ -31,26 +34,34 @@ def get_elastic_cert():
             f.write(response.content)
         return file
 
+
 # Configuration - Keys
 kms_key_id              = os.environ['KMS_KEY_ID']
 elastic_auth_pw_encr    = os.environ['ELK_PASS_ENCR']
 s3_key_encr             = os.environ['S3_KEY_ENCR']
 s3_secret_encr          = os.environ['S3_SECRET_ENCR']
 
+
 # Configuration - Setting up variables for ElasticSearch
-elastic_server      = os.environ['ELASTIC_SERVER']
-elastic_auth_user   = os.environ['ELASTIC_AUTH_USER']
-elastic_auth_pw     = decrypt_secret_kms(elastic_auth_pw_encr)
-elastic_index_name  = os.environ['ELASTIC_INDEX_NAME']
-kibana_server       = os.environ['KIBANA_SERVER']
-elastic_cert        = get_elastic_cert()
+elastic_server          = os.environ['ELASTIC_SERVER']
+elastic_auth_user       = os.environ['ELASTIC_AUTH_USER']
+elastic_auth_pw         = decrypt_secret_kms(elastic_auth_pw_encr)
+elastic_index_name      = f"{os.environ['ELASTIC_INDEX_NAME']}-000001"
+elastic_index_alias     = "audit-trails-index"
+elastic_index_template  = "audit-trails-template"
+elastic_index_ilm       = "audit-trails-ilm"
+elastic_index_pipeline  = "audit-trails-pipeline"
+kibana_server           = os.environ['KIBANA_SERVER']
+elastic_cert            = get_elastic_cert()
+
 
 # Configuration - Setting up variables for S3
-s3_key              = decrypt_secret_kms(s3_key_encr)
-s3_secret           = decrypt_secret_kms(s3_secret_encr)
-s3_bucket           = os.environ['S3_BUCKET']
-s3_folder           = os.environ['S3_FOLDER']
-s3_local            = '/tmp/s3'
+s3_key                  = decrypt_secret_kms(s3_key_encr)
+s3_secret               = decrypt_secret_kms(s3_secret_encr)
+s3_bucket               = os.environ['S3_BUCKET']
+s3_folder               = os.environ['S3_FOLDER']
+s3_local                = '/tmp/s3'
+
 
 # Configuration - Sleep time
 if(os.getenv('SLEEP_TIME') is not None):
@@ -58,20 +69,22 @@ if(os.getenv('SLEEP_TIME') is not None):
 else:
     sleep_time = 240
 
+
 # State - Setting up S3 client
 s3 = boto3.resource('s3',
-    endpoint_url='https://storage.yandexcloud.net',
-    aws_access_key_id = s3_key,
-    aws_secret_access_key = s3_secret 
+    endpoint_url            = 'https://storage.yandexcloud.net',
+    aws_access_key_id       = s3_key,
+    aws_secret_access_key   = s3_secret 
 )
+
 
 # Function - Create config index in ElasticSearch
 def create_config_index():
-    request_suffix = f"/.state-{elastic_index_name}"
-    response = requests.get(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    request_suffix  = f"/.state-{elastic_index_alias}"
+    response        = requests.get(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
     if(response.status_code == 404):
-        request_suffix = f"/.state-{elastic_index_name}/_doc/1"
-        request_json = """{
+        request_suffix  = f"/.state-{elastic_index_alias}/_doc/1"
+        request_json    = """{
             "is_configured": true
         }"""
         response = requests.post(elastic_server+request_suffix, data=request_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"Content-Type":"application/json"})
@@ -80,40 +93,91 @@ def create_config_index():
         print('Config index -- EXISTS')
     print(f"{response.status_code} - {response.text}")
 
+
 # Function - Get config index state
 def get_config_index_state():
-    request_suffix = f"/.state-{elastic_index_name}/_doc/1/_source"
-    response = requests.get(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    request_suffix  = f"/.state-{elastic_index_alias}/_doc/1/_source"
+    response        = requests.get(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
     if(response.status_code != 200):
         return False
     return response.json()['is_configured']
 
+
 # Function - Create ingest pipeline
 def create_ingest_pipeline():
-    request_suffix = '/_ingest/pipeline/audit-trails-pipeline'
-    data_file = open('/app/include/audit-trail/pipeline.json')
-    data_json = json.load(data_file)
+    request_suffix  = f"/_ingest/pipeline/{elastic_index_pipeline}"
+    data_file       = open('/app/include/audit-trail/pipeline.json')
+    data_json       = json.load(data_file)
     data_file.close()
-    response = requests.put(elastic_server+request_suffix, json=data_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    response        = requests.put(elastic_server+request_suffix, json=data_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
     if(response.status_code == 200):
         print('Ingest pipeline -- CREATED')
     print(f"{response.status_code} - {response.text}")
 
-# Function - Create an index with mapping
-def create_index_with_map():
-    request_suffix = f"/{elastic_index_name}"
-    data_file = open('/app/include/audit-trail/mapping.json')
-    data_json = json.load(data_file)
+
+# Function - Create an index template
+def create_index_template():
+    request_suffix  = f"/_template/{elastic_index_template}"
+    data_file       = open('/app/include/audit-trail/index-template.json')
+    data_json       = json.load(data_file)
     data_file.close()
-    response = requests.put(elastic_server+request_suffix, json=data_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    response        = requests.put(elastic_server+request_suffix, json=data_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
     if(response.status_code == 200):
-        print('Index with mapping -- CREATED')
+        print('Index template -- CREATED')
     print(f"{response.status_code} - {response.text}")
+
+
+# Function - Create an index lifecycle policy
+def create_lifecycle_policy():
+    request_suffix  = f"/_ilm/policy/{elastic_index_ilm}"
+    request_json    = """{
+        "policy": {
+            "phases": {
+                "hot": {
+                    "min_age": "0ms",
+                    "actions": {
+                        "rollover": {
+                            "max_age": "30d",
+                            "max_primary_shard_size": "50gb"
+                        }
+                    }
+                }
+            }
+        }
+    }"""
+    response = requests.put(elastic_server+request_suffix, data=request_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"Content-Type":"application/json"})
+    if(response.status_code == 200):
+        print('Index lifecycle policy -- CREATED')
+    print(f"{response.status_code} - {response.text}")
+
+
+# Function - Create an index
+def create_first_index():
+    request_suffix  = f"/{elastic_index_name}"
+    response        = requests.put(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    if(response.status_code == 200):
+        print(f"Index {elastic_index_name} -- CREATED")
+    print(f"{response.status_code} - {response.text}")
+
+
+# Function - Create an index alias
+def create_index_alias():
+    request_suffix  = f"/_aliases"
+    request_json    = """{
+        "actions" : [
+            { "add" : { "index" : "%s", "alias" : "%s" } }
+        ]
+    }""" % (elastic_index_name, elastic_index_alias)
+    response = requests.post(elastic_server+request_suffix, data=request_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"Content-Type":"application/json"})
+    if(response.status_code == 200):
+        print('Index alias -- CREATED')
+    print(f"{response.status_code} - {response.text}")
+
 
 # Function - Refresh index
 def refresh_index():
-    request_suffix = f"/{elastic_index_name}/_refresh"
-    response = requests.post(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    request_suffix  = f"/{elastic_index_alias}/_refresh"
+    response        = requests.post(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
     if(response.status_code == 200):
         print('Index -- REFRESHED')
     print(f"{response.status_code} - {response.text}")
@@ -190,6 +254,7 @@ def configure_kibana():
         print('Detections -- IMPORTED')
     print(f"{response.status_code} - {response.text}")
 
+
 # Function - Download JSON logs to local folder
 def download_s3_folder(s3_bucket, s3_folder, local_folder=None):
     print('JSON download -- STARTED')
@@ -207,6 +272,7 @@ def download_s3_folder(s3_bucket, s3_folder, local_folder=None):
         bucket.download_file(obj.key, local_folder+'/'+target.rsplit('/')[-1])
     print('JSON download -- COMPLETE')
 
+
 # Function - Clean up S3 folder
 def delete_objects_s3(s3_bucket, s3_folder):
     bucket = s3.Bucket(s3_bucket)
@@ -223,10 +289,11 @@ def delete_objects_s3(s3_bucket, s3_folder):
             )
     print('S3 bucket -- EMPTIED')
 
+
 # Function - Upload logs to ElasticSearch
 def upload_docs_bulk(s3_bucket, s3_folder):
     print('JSON upload -- STARTED')
-    request_suffix = f"/{elastic_index_name}/_bulk?pipeline=audit-trails-pipeline"
+    request_suffix = f"/{elastic_index_alias}/_bulk?pipeline={elastic_index_pipeline}"
     error_count = 0
 
     for f in os.listdir(s3_local):
@@ -245,12 +312,14 @@ def upload_docs_bulk(s3_bucket, s3_folder):
             if(response.status_code != 200):
                 error_count += 1
                 print(response.text)
+            print(f"{response.status_code} - {response.text}")
     if(os.path.exists(f"{s3_local}/nd-temp.json")):
         os.remove(f"{s3_local}/nd-temp.json")
     print(f"JSON upload -- COMPLETE -- {error_count} ERRORS")
     if(error_count == 0):
         delete_objects_s3(s3_bucket, s3_folder)
     refresh_index()
+
 
 # Process - Upload data
 def upload_logs():
@@ -259,15 +328,18 @@ def upload_logs():
         download_s3_folder(s3_bucket, s3_folder, s3_local)
         upload_docs_bulk(s3_bucket, s3_folder)
     else:
-        create_index_with_map()
+        create_lifecycle_policy()
+        create_index_template()
+        create_first_index()
+        create_index_alias()
         create_ingest_pipeline()
         configure_kibana()
         create_config_index()
         download_s3_folder(s3_bucket, s3_folder, s3_local)
         upload_docs_bulk(s3_bucket, s3_folder)
 
-### MAIN CONTROL PANEL
 
+### MAIN CONTROL PANEL
 upload_logs()
 print("Sleep -- STARTED")
 time.sleep(sleep_time)
