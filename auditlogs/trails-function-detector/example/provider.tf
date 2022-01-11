381cdc8e9da9028959b3a0de3e3ae453b3550cba
Mirtov Alexey <sobolevalexwee@yandex.ru>
2022-01-11 17:57:06 +0300

    Update provider.tf

--- auditlogs/trails-function-detector/example/provider.tf
+++ auditlogs/trails-function-detector/example/provider.tf
@@ -12,7 +12,7 @@ provider "yandex" {
   service_account_key_file = "./key.json"
   # or you can use: token = var.token for user account not sa
   cloud_id = "XXXXXX" #your cloud_id
-  folder_id = "XXXXXX" yout folder_id
+  folder_id = "XXXXXX" #your folder_id
   max_retries              = 10
 }
 
