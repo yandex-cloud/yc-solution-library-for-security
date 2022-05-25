a9d9d6ad4aab977ee61ba28b59b09fe7041983f6
Mirtov Alexey <sobolevalexwee@yandex.ru>
2022-05-25 14:36:50 +0300

    Update 03-infra.tf

--- auditlogs/export-auditlogs-to-ELK_k8s/modules/03-infra.tf
+++ auditlogs/export-auditlogs-to-ELK_k8s/modules/03-infra.tf
@@ -16,3 +16,14 @@ resource "yandex_iam_service_account_static_access_key" "sa_static_key" {
   service_account_id = data.yandex_iam_service_account.bucket_sa.id
   description        = "static access key for object storage"
 }
+
+data "yandex_client_config" "client" {}
+
+provider "helm" {
+  kubernetes {
+    host     = data.yandex_kubernetes_cluster.my_cluster.master.0.public_ip == true ?   data.yandex_kubernetes_cluster.my_cluster.master.0.external_v4_endpoint : data.yandex_kubernetes_cluster.my_cluster.master.0.internal_v4_endpoint 
+    cluster_ca_certificate = data.yandex_kubernetes_cluster.my_cluster.master.0.cluster_ca_certificate
+    token                  = data.yandex_client_config.client.iam_token
+
+  }
+}
