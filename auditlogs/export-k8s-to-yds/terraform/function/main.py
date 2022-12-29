fb0fe31dd990c8de9071dd7816bf40eb369f31a3
Andrey Lavretsky <alavret@hotmail.com>
2022-12-29 13:46:23 +0300

    change yds part of solution v1.1

--- auditlogs/export-k8s-to-yds/terraform/function/main.py
+++ auditlogs/export-k8s-to-yds/terraform/function/main.py
@@ -37,7 +37,7 @@ def handler(event, context):
     for log_data in event['messages']:
         for log_entry in  log_data['details']['messages']:
             push_to_kinesis.append({'Data': log_entry['message'],'PartitionKey': str(get_random_alphanumeric_string(5))} )
-            response = client.put_records(StreamName="/ru-central1/{folder}/{database}/{stream}".format(folder=folder_name, database=yds_ydb_id, stream=yds_name), Records=push_to_kinesis)
+    response = client.put_records(StreamName="/ru-central1/{folder}/{database}/{stream}".format(folder=folder_name, database=yds_ydb_id, stream=yds_name), Records=push_to_kinesis)
     num_of_records = len(push_to_kinesis)
     print(f'Records count - {num_of_records}')
     print(f'Response from YDS - {response}')
