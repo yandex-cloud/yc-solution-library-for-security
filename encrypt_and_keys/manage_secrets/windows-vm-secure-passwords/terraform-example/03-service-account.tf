5b1f0469510664533971d68fc425be2a4a8cac6a
epparfenov <epparfenov@yandex-team.ru>
2023-06-02 15:56:26 +0300

    Lockbox windows solution variables naming edit

--- encrypt_and_keys/manage_secrets/windows-vm-secure-passwords/terraform-example/03-service-account.tf
+++ encrypt_and_keys/manage_secrets/windows-vm-secure-passwords/terraform-example/03-service-account.tf
@@ -1,14 +1,14 @@
 # Creating Service Account
-resource "yandex_iam_service_account" "kc-sa" {
+resource "yandex_iam_service_account" "win-sa" {
   name        = "${var.sa_name}"
 }
 
 # Creating self admin binding for future self deletion
 resource "yandex_iam_service_account_iam_binding" "sa-self-binding" {
-  service_account_id = "${yandex_iam_service_account.kc-sa.id}"
+  service_account_id = "${yandex_iam_service_account.win-sa.id}"
   role               = "admin"
 
   members = [
-    "serviceAccount:${yandex_iam_service_account.kc-sa.id}",
+    "serviceAccount:${yandex_iam_service_account.win-sa.id}",
   ]
 }
